eee748ac4616a54cce74d7780d2d7c18d4ff25ff
Dancc1927 <148361573+Dancc1927@users.noreply.github.com>
2024-03-07 07:26:05 -0500

    Array fill

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ console.log(Constructor);
 let datos = ("perro", "gato", "ave");
 let result = datos.from({length:10},(_, index) => index + 1);
 console.log(result);
+
+let num = [1,2,3,4];
+console.log(num.fill(2,4));
