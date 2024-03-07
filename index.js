4a1dd68085bc190e3d337a6c6ebedaa41457b5e6
Dancc1927 <148361573+Dancc1927@users.noreply.github.com>
2024-03-07 07:14:51 -0500

    Constructor

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ let array = ["avion","ave","muro"];
 
 let arrayDatos = ["10", "Maria", true];
 console.log(arrayDatos);
+
+let Constructor = new array("rojo", "pez", "perro", 1,2,3);
+console.log(Constructor);
