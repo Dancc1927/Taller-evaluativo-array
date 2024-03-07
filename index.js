4c98adaa3e2c9aa9ec347711b6de4e671e761919
Dancc1927 <148361573+Dancc1927@users.noreply.github.com>
2024-03-07 07:20:28 -0500

    Array from

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ console.log(arrayDatos);
 
 let Constructor = new array("rojo", "pez", "perro", 1,2,3);
 console.log(Constructor);
+
+let datos = ("perro", "gato", "ave");
+let result = datos.from({length:10},(_, index) => index + 1);
+console.log(result);
